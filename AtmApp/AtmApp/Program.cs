fd3b353c93d4c0a7d43cc9779ddd173d105395fd
Clarice Costello <claricostello@outlook.com>
2019-01-09 11:54:31 -0800

    Wrote out user prompt

---
+++ AtmApp/AtmApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace AtmApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Interface();
+        }
+        static void Interface()
+        {
+            Console.WriteLine("What would you like to do today? ( 1/2/3/4)");
+            Console.WriteLine("1. View Balance");
+            Console.WriteLine("2. Deposit Money");
+            Console.WriteLine("3. Withdraw Money");
+            Console.WriteLine("4. Exit");
+            Console.ReadLine();
+
+        }
+    }
+}
