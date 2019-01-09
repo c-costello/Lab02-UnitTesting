1caa58a71e149f5a1eefe51b207116c03ae3675b
Clarice Costello <claricostello@outlook.com>
2019-01-09 12:55:16 -0800

    wrote CanAddTwoNumbers test for AddMoney method

--- AtmApp/AtmTests/UnitTest1.cs
+++ AtmApp/AtmTests/UnitTest1.cs
@@ -6,20 +6,36 @@ namespace AtmTests
 {
     public class UnitTest1
     {
+        //AddMoney tests
+        [Fact]
+        public void CanAddTwoNumbers()
+        {
+            Program.balance = 5000;
+            Assert.Equal(6000, Program.AddMoney(1000));
+        }
+
         [Fact]
         public void CannotAddNegatives()
         {
+            Program.balance = 5000;
             Assert.Equal(5000, Program.AddMoney(-500));
         }
+
+        //SubtractMoney tests
         [Fact]
         public void CannotWithdrawNegatives()
+
         {
+            Program.balance = 5000;
             Assert.Equal(5000, Program.SubtractMoney(-500));
         }
         [Fact]
         public void CannotWithdrawMoreThanBalance()
         {
+            Program.balance = 5000;
             Assert.Equal(5000, Program.SubtractMoney(6000));
         }
+
+        //UserInput tests
     }
 }
